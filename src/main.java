0cc967df3f73ab808b78ac34f62a282bf379d51c
AaronFrancis05 <aarontaremwa8@gmail.com>
2026-03-02 04:11:20 +0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(420,420);
+        frame.setTitle("My first Frame");
+        frame.setVisible(true);
+    }
+}
